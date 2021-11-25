c5e323d6ffd1b9d892f81f08825fdc18d0ff112a
syd775 <sydney95174@gmail.com>
2021-11-25 18:34:14 -0500

    Initial Commit

---
+++ src/Gui.java
@@ -0,0 +1,15 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+
+public class Gui {
+    public static void main(String args[]){
+        JFrame mainFrame = new JFrame("My First GUI");
+        mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        mainFrame.setSize(900, 700);
+
+
+        mainFrame.setVisible(true);
+    }
+}
